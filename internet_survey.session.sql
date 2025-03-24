6898de1dc43e347d25dfc8556aeada10c5202fcd
kingksjo <kamiye06@gmail.com>
2025-03-24 16:26:02 +0100

    Cleaned and Normalized the data

--- internet_survey.session.sql
+++ internet_survey.session.sql
@@ -2,3 +2,6 @@
 -- @block
 SELECT gender, residence FROM survey_responses WHERE residence = "Agric Quarters";
 
+-- @block
+SELECT * FROM survey_responses WHERE monthly_data_usage_gb = "10 - 20 GB";
+
